0e78263a93dd94049057adaf1c01d144a3831a9b
manojconcept <manojveerapavu@gmail.com>
2024-08-01 13:17:50 +0530

    {file-stru}

--- stdclass/stdclass.php
+++ stdclass/stdclass.php
@@ -6,23 +6,35 @@ error_reporting(E_ALL);
 //string
 $person = '{"first_name":"john","age":"30"}'; //json string
 echo $person;
-echo "<br>";
+echo "<hr>";
 //echo json_decode($person) // Object of class stdClass could not be converted to string
 //print(json_decode($person)); // Object of class stdClass could not be converted to string
 json_decode($person); // converting the json string to an array
 print_r(json_decode($person)); //stdClass Object ( [first_name] => john [age] => 30 )
-echo '<br>';
+echo '<hr>';
 print_r((object)$person); // stdClass Object ( [scalar] => {"first_name":"john","age":"30"} )
-echo "<br>";
+echo "<hr>";
 print_r(json_decode($person, true)); // Array ( [first_name] => john [age] => 30 )
-echo "<br>";
+echo "<hr>";
 var_dump(json_decode($person));
-echo "<br>";
+echo "<hr>";
 var_dump(array("manoj","super","jani")); //array(3) { [0]=> string(5) "manoj" [1]=> string(5) "super" [2]=> string(4) "jani" }
-echo "<br>";
+echo "<hr>";
 var_dump(array("name"=>"manoj","codename" => "mm")); //array(2) { ["name"]=> string(5) "manoj" ["codename"]=> string(2) "mm" }
-echo "<br>";
+echo "<hr>";
 //type casting converting an array into object
 var_dump((object)array("name"=>"manoj","codename" => "mm")); // object(stdClass)#1 (2) { ["name"]=> string(5) "manoj" ["codename"]=> string(2) "mm" }
-echo "<br>";
+echo "<hr>";
 var_dump((object)$person);
+//creating an object without class
+// new stdClass();
+echo "<hr>";
+$person = new \stdClass();
+$person->name = "manojconcept";
+$person -> skintone = "green";
+print_r($person);
+echo "<hr>";
+var_dump($person);
+echo "<hr>";
+var_dump((array)$person);
+echo "<hr>";
